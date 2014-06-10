7dc95b7d5877ea43118c5292164b4e1ca2dfb114
nandinivelmurugan <nandinivel@hotmail.com>
2014-06-10 15:33:41 +0800

    splash screen

---
+++ nandini/IntroScreen/src/com/nandini/introscreen/MainActivity.java
@@ -0,0 +1,51 @@
+package com.nandini.introscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	
+	int counter;
+	
+	Button add, sub;
+	TextView display;
+	
+	public void updateDisplay(){
+		this.display.setText("Your total is: " + this.counter);
+	}
+	
+	
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+        
+       
+        counter = 0;
+        add = (Button) findViewById(R.id.bAdd);
+        sub = (Button) findViewById(R.id.bSub);
+        display = (TextView) findViewById(R.id.tvDisplay);
+        
+        add.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				counter++;
+				display.setText("Your total is: " + counter);
+			}
+		});
+        
+        sub.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				counter--;
+				display.setText("Your total is: " + counter);
+			}
+		});
+        
+    }
+}
---
+++ nandini/IntroScreen/src/com/nandini/introscreen/Splash.java
@@ -0,0 +1,30 @@
+package com.nandini.introscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openStartingPoint = new Intent("com.nandini.splashscreen.MAINACTIVITY");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		
+		timer.start();
+	}
+	
+}
