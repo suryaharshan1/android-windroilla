60f576f9f1eed4a92a9b93bb218e142500dd3aec
nandinivelmurugan <nandinivel@hotmail.com>
2014-07-01 23:44:21 +0800

    ChangingWallpaper(Bitmap)

--- nandini/IntroScreen/src/com/nandini/introscreen/Camera.java
+++ nandini/IntroScreen/src/com/nandini/introscreen/Camera.java
@@ -1,13 +1,75 @@
 package com.nandini.introscreen;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class Camera extends Activity {
+public class Camera extends Activity implements View.OnClickListener{
 
+	Button b;
 	ImageButton ib;
 	ImageView iv;
-	Button b;
+	
+	Intent i;
+	final static int cameraData = 0;
+	Bitmap bmp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		// custom
+		setContentView(R.layout.photo);
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		b = (Button) findViewById(R.id.bSetWallpaper);
+		ib = (ImageButton) findViewById(R.id.ibTakePic);
+		iv = (ImageView) findViewById(R.id.ivReturnedPic);
+		
+		b.setOnClickListener(this);
+		ib.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.ibTakePic:
+			i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(i, cameraData);
+			
+			break;
+		case R.id.bSetWallpaper:
+			try {
+				getApplicationContext().setWallpaper(bmp);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		// custom
+		if(resultCode == RESULT_OK){
+			Bundle extras = data.getExtras();
+			bmp = (Bitmap) extras.get("data");
+			iv.setImageBitmap(bmp);
+		}
+	}
+	
+	
+
 }
